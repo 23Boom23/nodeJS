991be0c75ae9a19ad5a9380837af32786ac7756b
garus_ae <garus.ae@weldbook.ru>
2022-09-15 23:19:33 +0300

    feat:connect data.json file to a user service

--- services/users.service.js
+++ services/users.service.js
@@ -1,21 +1,22 @@
-
-// let users = [ { id: 1, name: "Pasha", isMan: true, age: 25 },
-// { id: 2, name: "Lesha", isMan: true, age: 28 },
-// { id: 3, name: "Masha", isMan: false, age: 32 },
-// { id: 4, name: "Marina", isMan: false, age: 51 }
-// ]
-    class UsersService{
-        getUsers() {
-            return new Promise((res,rej) => {
-                res([ { id: 1, name: "Pasha", isMan: true, age: 25 },
-                { id: 2, name: "Lesha", isMan: true, age: 28 },
-                { id: 3, name: "Masha", isMan: false, age: 32 },
-                { id: 4, name: "Marina", isMan: false, age: 51 }
-                ])
+const fs = require('fs')
+class UsersService {
+    getUsers() {
+        try {
+            return new Promise((res, rej) => {
+                fs.readFile("data.json", (err, data) => {
+                    if (err) {
+                        return res(false);
+                    }
+                    return res(JSON.parse(data))
+                })
             })
+        } catch (err) {
+            console.log(err)
         }
-
     }
+}
 
 
 module.exports = new UsersService()
+
+
